3d614cbe455030314db67b3b5cbf91a5ee1af60a
yardalgedal <yardalgedal@gmail.com>
2019-04-30 14:34:40 +0300

    Remove duplications

--- app/http.py
+++ app/http.py
@@ -6,18 +6,16 @@ __all__ = ['new', 'search', 'get']
 
 
 async def new(request: web.Request) -> web.Response:
-    return web.json_response(text=str(await actions.new(
-        request.app['db'], request.query.get('text'), request.query.get('parent')
-    )))
+    return await make_response(actions.new, request.app['db'], request.query.get('text'), request.query.get('parent'))
 
 
 async def search(request: web.Request) -> web.Response:
-    return web.json_response(text=str(await actions.search(
-        request.app['db'], request.query.get('text')
-    )))
+    return await make_response(actions.search, request.app['db'], request.query.get('text'))
 
 
 async def get(request: web.Request) -> web.Response:
-    return web.json_response(text=str(await actions.get(
-        request.app['db'], request.query.get('id')
-    )))
+    return await make_response(actions.get, request.app['db'], request.query.get('id'))
+
+
+async def make_response(f, *args, **kwargs):
+    return web.json_response(text=str(await f(*args, **kwargs)))
